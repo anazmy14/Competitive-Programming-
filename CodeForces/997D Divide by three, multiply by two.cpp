8dae5bb3ee2c670c10fa76682fcbbaf6e8aeb028
ahmed <you@example.com>
2020-05-30 18:11:50 +0200

    997D

--- CodeForces/997D Divide by three, multiply by two.cpp
+++ CodeForces/997D Divide by three, multiply by two.cpp
@@ -17,14 +17,56 @@
 	#define MKP make_pair 
     #define ull unsigned long long
 	using namespace std;
-
-
+ 
+	int gcd(int a, int b)
+	{
+		if (a == 0)
+			return b;
+		return gcd(b % a, a);
+	}
+ 
+	int lcm(int a, int b)
+	{
+		return (a*b)/gcd(a, b);
+	}
+ 
+	int fact(int n) {
+		if (n == 0 || n == 1)
+			return 1;
+		else
+			return n * fact(n - 1);
+	}
+ 
+ 
+	int cnt (ull x ){
+		int c = 0 ;
+		while( x%3 == 0) {
+			c--;
+			x/=3 ;
+		}
+		return c; 
+	}
  
 	int main () {
 		ios_base::sync_with_stdio(false);
 		cin.tie(NULL);
-		ull n ; cin >> n ;
-
+		int n ; cin >> n ;
+		vector <pair< int , ull >> v(n);
+ 
+		for( int i=0; i<n;i++ ) {
+			cin >> v[i].second; 
+			v[i].first = cnt(v[i].second) ;			
+		}
+ 
+		sort( v.begin() , v.end() );
+ 
+		for ( pair < ull , int > x : v )
+			cout << x.second << " "; 
+ 
+	
+ 
+ 
+ 
  
 		return 0 ;
  
