16da1dd734f850c4f4e151ef6b4ff317ced110e7
ahmed <you@example.com>
2020-06-20 23:18:53 +0200

    1370A

---
+++ CodeForces/1370A Maximum GCD.cpp
@@ -0,0 +1,140 @@
+#include<iostream> 
+#include <vector> 
+#include <string>
+#include <map>
+#include <algorithm> 
+#include <deque> 
+#include <set>
+#include <cmath>
+#define MKP make_pair
+#define ull unsigned long long
+using namespace std;
+ 
+ 
+ 
+ull gcd(ull a, ull b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+ 
+ull lcm(ull a, ull b)
+{
+    return (a*b)/gcd(a, b);
+}
+ 
+ 
+ 
+ull nCr(ull n, ull r)
+{
+ 
+    ull p = 1, k = 1;
+ 
+ 
+    if (n - r < r)
+        r = n - r;
+ 
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+ 
+            ull m = gcd(p, k);
+ 
+ 
+            p /= m;
+            k /= m;
+ 
+            n--;
+            r--;
+        }
+ 
+    }
+ 
+    else
+        p = 1;
+ 
+    return p ;
+}
+ 
+ull power ( ull b , ull p ) {  
+	
+	if (!p) 
+		return 1 ;
+	if ( p%2)
+		return b * power (b , p-1 ) ;
+	
+	return power(b , p/2) * power(b , p/2) ;   
+}
+ 
+struct dsu {
+	int n ;
+	vector<int> rank ;
+	vector<int> parent ;
+	vector<int> sz ; 
+	dsu ( int x) {
+		n = x ;
+		for ( int i= 0 ; i <= n ; i++ ) {
+			parent.push_back(i) ;
+			rank.push_back(1) ;
+			sz.push_back(1) ;
+		}
+	}
+ 
+	int find (int x) {
+		if ( parent[x] == x )
+			return x ;
+		return parent[x] = find ( parent[x] ) ;
+	}
+ 
+	void link ( int x , int y ) {
+		int px = find(x) , py = find(y) ;
+		if ( px == py ) 
+			return ;
+		
+		if ( rank[px] > rank[py] ) {
+			parent[py] = px ; 
+			sz[px] += sz[py];
+		}
+		else if ( rank [py] > rank [px] ) {
+			parent[px] = py ;
+			sz[py] += sz[px] ;
+		}
+ 
+		else {
+			rank[px]++ ;
+			sz[px] += sz[py] ;
+			parent[py]= px ;
+		}
+	}
+ 
+ 
+ 
+};
+ 
+ 
+ 
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t; cin >> t ;
+	while( t--){
+		int n ; cin >> n ;
+ 
+		if ( n % 2 ) n-- ;
+ 
+		cout << n/2 << endl  ; 
+ 
+ 
+		
+	}
+ 
+ 
+ 
+ 
+	
+		 
+ 
+ return 0 ; 
+}
