e647af53365d1729f61b908f557a1b29374a6420
ahmed <you@example.com>
2020-06-21 22:41:14 +0200

    1144D

---
+++ CodeForces/1144D Equalize Them All.cpp
@@ -0,0 +1,170 @@
+#include<iostream> 
+#include <vector> 
+#include <string>
+#include <map>
+#include <algorithm> 
+#include <deque> 
+#include <set>
+#include <cmath>
+#define MKP make_pair
+#define ull unsigned long long
+using namespace std;
+ 
+ 
+ 
+ull gcd(ull a, ull b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+ 
+ull lcm(ull a, ull b)
+{
+    return (a*b)/gcd(a, b);
+}
+ 
+ 
+ 
+ull nCr(ull n, ull r)
+{
+ 
+    ull p = 1, k = 1;
+ 
+ 
+    if (n - r < r)
+        r = n - r;
+ 
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+ 
+            ull m = gcd(p, k);
+ 
+ 
+            p /= m;
+            k /= m;
+ 
+            n--;
+            r--;
+        }
+ 
+    }
+ 
+    else
+        p = 1;
+ 
+    return p ;
+}
+ 
+ull power ( ull b , ull p ) {  
+	
+	if (!p) 
+		return 1 ;
+	if ( p%2)
+		return b * power (b , p-1 ) ;
+	
+	return power(b , p/2) * power(b , p/2) ;   
+}
+ 
+struct dsu {
+	int n ;
+	vector<int> rank ;
+	vector<int> parent ;
+	vector<int> sz ; 
+	dsu ( int x) {
+		n = x ;
+		for ( int i= 0 ; i <= n ; i++ ) {
+			parent.push_back(i) ;
+			rank.push_back(1) ;
+			sz.push_back(1) ;
+		}
+	}
+ 
+	int find (int x) {
+		if ( parent[x] == x )
+			return x ;
+		return parent[x] = find ( parent[x] ) ;
+	}
+ 
+	void link ( int x , int y ) {
+		int px = find(x) , py = find(y) ;
+		if ( px == py ) 
+			return ;
+		
+		if ( rank[px] > rank[py] ) {
+			parent[py] = px ; 
+			sz[px] += sz[py];
+		}
+		else if ( rank [py] > rank [px] ) {
+			parent[px] = py ;
+			sz[py] += sz[px] ;
+		}
+ 
+		else {
+			rank[px]++ ;
+			sz[px] += sz[py] ;
+			parent[py]= px ;
+		}
+	}
+ 
+ 
+ 
+};
+
+
+ 
+ 
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	
+	int n , c = 0 , x , p ; cin >> n ;
+	vector<int> v(n) ; 
+	map<int ,int > m ;
+	for ( int i = 0 ; i < n ; i++ ) {
+		cin >> v[i] ;
+		m[v[i]]++ ;
+
+		if ( m[v[i]] > c ) {
+			c = m[v[i]] ; 
+			x = v[i] ;
+			p = i ;
+		}
+	}
+
+	cout << n-c << endl ;
+
+	for ( int i = p ; i < n ; i++ ) {
+		if ( v[i] == x )
+			continue ;
+
+		if ( v[i] < x ) 
+			cout << 1  << " "  << i+1 << " " << i ;
+		else 
+			cout << 2 << " " << i+1 << " " << i ; 
+		cout << endl ;
+	}
+
+	for ( int i = p ; i >= 0 ; i-- ) {
+		if ( v[i] == x )
+			continue ;
+
+		if ( v[i] < x ) 
+			cout << 1  << " "  << i+1 << " " << i+2 ;
+		else 
+			cout << 2 << " " << i+1 << " " << i+2 ; 
+		cout << endl ;
+	}
+
+
+
+
+	
+
+
+		 
+ 
+ return 0 ; 
+}
