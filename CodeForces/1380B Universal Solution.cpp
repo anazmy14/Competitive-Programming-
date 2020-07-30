a5035bd07db1f8d4e9888ae26ca9e90e20be9252
ahmed <you@example.com>
2020-07-30 22:11:03 +0200

    1380B

---
+++ CodeForces/1380B Universal Solution.cpp
@@ -0,0 +1,155 @@
+#include<iostream> 
+#include <vector> 
+#include <string>
+#include <map>
+#include <algorithm> 
+#include <deque>
+#include<queue> 
+#include <set>
+#include <cmath>
+#define MKP make_pair
+#define ull unsigned long long
+#define ll long long
+using namespace std;
+ 
+ 
+ 
+ull gcd(ull a, ull b)
+{
+    if (a == 0)
+        return b;
+	if (b == 0 ) 
+		return a ;
+    return gcd(b % a, a);
+}
+ 
+ull lcm(ull a, ull b)
+{
+    return (a*b)/gcd(a, b);
+}
+ 
+ 
+ 
+ull nCr(ull n, ull r)
+{
+ 
+    ull p = 1, k = 1;
+ 
+ 
+    if (n - r < r)
+        r = n - r;
+ 
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+ 
+            ull m = gcd(p, k);
+ 
+ 
+            p /= m;
+            k /= m;
+ 
+            n--;
+            r--;
+        }
+ 
+    }
+ 
+    else
+        p = 1;
+ 
+    return p ;
+}
+ 
+long long power (long long b , long long p , long long m ) {  
+	
+	if (!p) 
+		return 1 ;
+	if ( p%2)
+		return (b * power (b , p-1 , m ))%m ;
+	
+	ull  x = power(b , p/2 , m ) ;   
+ 
+	return (x * x)%m ;   
+}
+ 
+struct dsu {
+	int n ;
+	vector<int> rank ;
+	vector<int> parent ;
+	vector<int> sz ; 
+	dsu ( int x) {
+		n = x ;
+		for ( int i= 0 ; i <= n ; i++ ) {
+			parent.push_back(i) ;
+			rank.push_back(1) ;
+			sz.push_back(1) ;
+		}
+	}
+ 
+	int find (int x) {
+		if ( parent[x] == x )
+			return x ;
+		return parent[x] = find ( parent[x] ) ;
+	}
+ 
+	void link ( int x , int y ) {
+		int px = find(x) , py = find(y) ;
+		if ( px == py ) 
+			return ;
+		
+		if ( rank[px] > rank[py] ) {
+			parent[py] = px ; 
+			sz[px] += sz[py];
+		}
+		else if ( rank [py] > rank [px] ) {
+			parent[px] = py ;
+			sz[py] += sz[px] ;
+		}
+ 
+		else {
+			rank[px]++ ;
+			sz[px] += sz[py] ;
+			parent[py]= px ;
+		}
+	}
+ 
+ 
+ 
+};
+
+ 
+ 
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t; cin >> t ;
+	while(t--){
+		string z ;
+		cin >> z ;
+		int r , p ,s  ;
+		r = p = s = 0 ;
+
+		for( int i=0; i < z.length(); i++) {
+			if(z[i] == 'R') r++ ;
+			else if(z[i] == 'P') p++ ;
+			else s++ ;
+		}
+
+		if( s >=p && s >= r) 
+			cout << string ( z.length() , 'R' ); 
+		else if( p >=s && p >= r ) 
+			cout << string( z.length() , 'S' ) ;
+		else 
+			cout << string (z.length() , 'P' ) ;
+		cout << endl ; 
+
+
+
+
+	}
+
+
+ return 0 ;
+}
---
+++ CodeForces/1380C Create The Teams.cpp
@@ -0,0 +1,150 @@
+#include<iostream> 
+#include <vector> 
+#include <string>
+#include <map>
+#include <algorithm> 
+#include <deque>
+#include<queue> 
+#include <set>
+#include <cmath>
+#define MKP make_pair
+#define ull unsigned long long
+#define ll long long
+using namespace std;
+ 
+ 
+ 
+ull gcd(ull a, ull b)
+{
+    if (a == 0)
+        return b;
+	if (b == 0 ) 
+		return a ;
+    return gcd(b % a, a);
+}
+ 
+ull lcm(ull a, ull b)
+{
+    return (a*b)/gcd(a, b);
+}
+ 
+ 
+ 
+ull nCr(ull n, ull r)
+{
+ 
+    ull p = 1, k = 1;
+ 
+ 
+    if (n - r < r)
+        r = n - r;
+ 
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+ 
+            ull m = gcd(p, k);
+ 
+ 
+            p /= m;
+            k /= m;
+ 
+            n--;
+            r--;
+        }
+ 
+    }
+ 
+    else
+        p = 1;
+ 
+    return p ;
+}
+ 
+long long power (long long b , long long p , long long m ) {  
+	
+	if (!p) 
+		return 1 ;
+	if ( p%2)
+		return (b * power (b , p-1 , m ))%m ;
+	
+	ull  x = power(b , p/2 , m ) ;   
+ 
+	return (x * x)%m ;   
+}
+ 
+struct dsu {
+	int n ;
+	vector<int> rank ;
+	vector<int> parent ;
+	vector<int> sz ; 
+	dsu ( int x) {
+		n = x ;
+		for ( int i= 0 ; i <= n ; i++ ) {
+			parent.push_back(i) ;
+			rank.push_back(1) ;
+			sz.push_back(1) ;
+		}
+	}
+ 
+	int find (int x) {
+		if ( parent[x] == x )
+			return x ;
+		return parent[x] = find ( parent[x] ) ;
+	}
+ 
+	void link ( int x , int y ) {
+		int px = find(x) , py = find(y) ;
+		if ( px == py ) 
+			return ;
+		
+		if ( rank[px] > rank[py] ) {
+			parent[py] = px ; 
+			sz[px] += sz[py];
+		}
+		else if ( rank [py] > rank [px] ) {
+			parent[px] = py ;
+			sz[py] += sz[px] ;
+		}
+ 
+		else {
+			rank[px]++ ;
+			sz[px] += sz[py] ;
+			parent[py]= px ;
+		}
+	}
+ 
+ 
+ 
+};
+
+ 
+ 
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	int t; cin >> t ;
+	while(t--){
+		ll n , x , c =0 , s = 1  ; cin >> n >> x ;
+		vector<ll> v(n); 
+		for(int i=0; i<n; i++) 
+			cin >> v[i] ;
+
+		sort(v.rbegin() , v.rend());
+		
+		for( int i =0 ; i <n ;i++) {
+			if( s* v[i] >= x ) {
+				c++ ;
+				s = 1 ;
+			}
+			else 
+				s++ ;
+		}
+
+		cout << c << endl ;
+	}
+
+
+ return 0 ;
+}
